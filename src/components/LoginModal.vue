91fc1f12b7a21ca2788c6b5fdedfb16dd8ddfba6
Vernon Cuipers <vcu14@uclive.ac.nz>
2018-05-28 23:23:26 +1200

    Created the modal for logging in a user, need to implement the login functionality

--- src/components/LoginModal.vue
+++ src/components/LoginModal.vue
@@ -11,14 +11,46 @@
           <div class="field">
             <label class="label is-3">Given Name</label>
             <div class="control">
-              <input class="input is-1" type="text" placeholder="Name and Surname">
+              <input class="input is-1" type="text" placeholder="First name(s)" v-bind="user.name">
             </div>
           </div>
-
+          <div class="field">
+            <label class="label is-3">Family Name</label>
+            <div class="control">
+              <input class="input is-1" type="text" placeholder="Family name" v-bind="user.familyName">
+            </div>
+          </div>
+          <div class="field">
+            <label class="label">Username</label>
+            <div class="control has-icons-left">
+              <input class="input" type="text" placeholder="Enter username" v-bind="user.username">
+              <span class="icon is-small is-left">
+                <i class="fa fa-user"></i>
+              </span>
+            </div>
+          </div>
+          <div class="field">
+            <label class="label">E-mail Address</label>
+            <div class="control has-icons-left">
+              <input class="input" type="text" placeholder="Enter e-mail address" v-bind="user.email">
+              <span class="icon is-small is-left">
+                <i class="fas fa-envelope"></i>
+              </span>
+            </div>
+          </div>
+          <div class="field">
+            <label class="label is-3">Enter password</label>
+            <div class="control">
+              <input class="input" type="password" placeholder="" v-bind="user.password">
+            </div>
+            <div>
+              <input class="input" type="password" placeholder="confirm password" v-bind="passMatch">
+            </div>
+          </div>
         </section>
         <footer class="modal-card-foot">
-          <button class="button is-success">Save changes</button>
-          <button class="button">Cancel</button>
+          <button class="button is-success">Register</button>
+          <button class="button" @click="$emit('close')">Cancel</button>
         </footer>
       </div>
     </div>
@@ -27,7 +59,19 @@
 
 <script>
   export default {
-    name: "loginModal"
+    name: "loginModal",
+    data () {
+      return {
+        user: {
+          name: "",
+          familyName: "",
+          username: "",
+          email: "",
+          password: ""
+        },
+        passMatch: ""
+      }
+    }
   }
 </script>
 
